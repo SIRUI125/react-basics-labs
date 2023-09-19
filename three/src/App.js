44aaa4a839102bb2548850549edfc3e77e0df5a9
Sirui Yao <20095250@mail.wit.ie>
2023-09-19 22:12:54 +0100

    Completed Week 2 Lab

--- three/src/App.js
+++ three/src/App.js
@@ -1,18 +1,34 @@
 import logo from './logo.svg';
 import './App.css';
 import Task from './components/Task';
+import React, {useState} from 'react';
+
 function App() {
+
+  const [ taskState, setTaskState ] = useState({
+    tasks: [
+      { id: 1, title:"Dishes", description: "Empty dishwasher", deadline: "Today" },
+      { id: 2, title: "Laundry", description: "Fold clothes and put away", deadline: "Tomorrow" },
+      { id: 3, title: "Tidy up", deadline: "Today" },
+      { id: 4, title: "Clean", description:"Clean the room", deadline:"Today"},
+      { id: 5, title: "Weeding", deadline:"Tomorrow"}
+    ]
+  });
+
   return (
     <div className="container">
-     <h1>three</h1>
-     <Task title="Dishes" deadline="Today"/>
-     <Task title="Laundry" deadline="Tomorrow">
-      Fold laundry and put away
-     </Task>
-     <Task title="Tidy" deadline="Today"></Task>
+      <h1>Tasky</h1>
+      
+      {taskState.tasks.map((task) => (              
+    <Task 
+      title={task.title}
+      description={task.description}
+      deadline={task.deadline}
+      key={task.id}
+    />
+  ))} 
     </div>
   );
 }
 
 export default App;
-
